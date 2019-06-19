eaf069084cdcb1ac26fd45555f10723da90e4254
gwencitaaa <gwendolen.warnick@gmail.com>
2019-06-19 11:28:02 +1000

    added models and views

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 // const feed = require('feed').Feed;
 const routes = require('./routes/routes');
-const News = require('./models/news');
-const PORT = 3000
+// const News = require('./models/news');
 
 const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', routes);
+app.use(logger('dev'));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
 
-mongoose.connect('mongodb://localhost:27017/newsfeed_app', {
-    useNewUrlParser: true
-}).then(function() {
-    console.log('Database connected. ');
+app.use(express.static(process.cwd() + '/public'));
+
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Listening on PORT' + port);
+}) ;
+    
+// app.use('/', routes);
+    
+mongoose.connect('mongodb://localhost/newsfeed_app');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+    console.log('Connected to Mongoose!');
 });
 
-app.listen(PORT, () => {
-    console.log('Listening on port ${PORT}')
-});
+// , {
+//      useNewUrlParser: true
+// }).then(function() {
+//     console.log('Database connected. ');
+// });
